dca850ba95b2c8ab56079d69ce3f560a46daf5b9
Nail Gimaev <gimntut@list.ru>
2017-03-19 16:36:08 +0500

    Fix setup.py

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ setup(
     packages=find_packages(),
     long_description=open(join(dirname(__file__), 'README.md')).read(),
     install_requires=[
-        'pystache==0.5.4'
+        'pystache==0.5.4',
         'nail.ssg.base==0.1.0',
         'ruamel.yaml==0.13.14'
     ],

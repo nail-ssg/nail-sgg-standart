3cdbaead0304780794fe34af392f4d70778055f9
Nail Gimaev <gimntut@list.ru>
2017-06-08 17:19:32 +0500

    rename mixin as loads

--- nail_ssg_standard/modules/loads.py
+++ nail_ssg_standard/modules/loads.py
@@ -4,45 +4,45 @@ from nail_ssg_base.modules.baseplugin import BasePlugin
 # from nail_ssg_base.prints import *
 
 
-class Mixin(BasePlugin):
+class Loads(BasePlugin):
     _default_config = {
         'scan': {
-            'order': ['nail_ssg_standard.mixin']
+            'order': ['nail_ssg_standard.loads']
         },
         'modify': {
-            'order': ['nail_ssg_standard.mixin']
+            'order': ['nail_ssg_standard.loads']
         }
     }
     _config_comments = {}
 
     def init(self):
-        self.config.mixins = []
+        self.config.loads = []
 
     def process_file(self, fileinfo, rules, data):
-        if '$mixin' in data:
-            self.config.mixins += [data]
+        if '$load' in data:
+            self.config.loads += [data]
         pass
 
     def modify_data(self):
-        for data in self.config.mixins:
-            mixins = data.get('$mixin', [])
-            if mixins == []:
+        for data in self.config.loads:
+            loads = data.get('$load', [])
+            if loads == []:
                 continue
             result = {}
-            for mixin in mixins:
-                d = copy.deepcopy(self.config.get_data(mixin))
+            for load in loads:
+                d = copy.deepcopy(self.config.get_data(load))
                 del d['$computed']
                 if 'abstract' in d['$global']:
                     del d['$global']['abstract']
                 result = dict_enrich(result, d)
             result = dict_enrich(data, result)
             data.update(result)
-            del data['$mixin']
-        del self.config.mixins
+            del data['$load']
+        del self.config.loads
 
     def build(self):
         pass
 
 
 def create(config):
-    return Mixin(config)
+    return load(config)
